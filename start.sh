56e351cd7666dfe2e9821d8fb07527c69efd2f6d
Nolan Heimlich <76967780+nheimlich@users.noreply.github.com>
2024-09-09 10:47:27 -0400

    adding updated start.sh for more posix compliance

--- start.sh
+++ start.sh
@@ -1,15 +1,7 @@
-#!/usr/bin/env bash
-account=$(triton account get | grep -e 'id:' | sed -e 's/id:\ //') # account UUID
-network=$(triton network ls -Hoid public=false)                    # Fabric Network UUID
-kubernetes_version="1.29.8"
-cluster_id=$(uuidgen | cut -d - -f1 | tr '[:upper:]' '[:lower:]')
-prd_params="-b bhyve --cloud-config configs/cloud-init -t cluster=$cluster_id -m cluster=$cluster_id -m account=$account -m k8ver=$kubernetes_version"
-dev_params="-b bhyve --cloud-config configs/cloud-init -t cluster=$cluster_id -m cluster=$cluster_id -m account=$account -m k8ver=$kubernetes_version"
+#!/usr/bin/env sh
 
-command -v triton >/dev/null 2>&1 && command -v fzf >/dev/null 2>&1 || {
-	echo >&2 "I require both the triton cli and fzf before running."
-	exit 1
-}
+command -v triton >/dev/null 2>&1 || echo >&2 "I require the triton cli before running"
+command -v fzf >/dev/null 2>&1 || echo >&2 "I require the fzf cli before running."
 
 usage() {
 	echo "Usage: $0 <action> [OPTIONS]"
@@ -18,10 +10,10 @@ usage() {
 }
 
 suffix() {
-	echo "For an instance suffix, please type it now (Example: {{shortId}}.suffix), or press enter to skip:"
+	printf "For an instance suffix, please type it now (Example: {{shortId}}.suffix), or press enter to skip:\n"
 	read -r suffix
 
-	if [ -z $suffix ]; then
+	if [ -z "$suffix" ]; then
 		echo "Defaulting to no suffix..."
 		name_modifier=""
 	else
@@ -30,56 +22,58 @@ suffix() {
 }
 
 ctr() {
-	local output=""
+	output=""
 	echo "creating control-plane members:"
 
-	output+=$(triton inst create -n {{shortId}}$name_modifier $image $ctr_package $prd_params -t triton.cns.services="init-$cluster_id,ctr-$cluster_id" -m "ctr_count=$num_ctr" -m "wrk_count=$num_wrk" -m tag="init" &)
+	output+=$(triton inst create -n {{shortId}}"$name_modifier" "$image" "$ctr_package" $prd_params -t triton.cns.services="init-$cluster_id,ctr-$cluster_id" -m "ctr_count=$num_ctr" -m "wrk_count=$num_wrk" -m tag="init" &)
 	output+="\n"
 
 	num_ctr=$((num_ctr - 1))
 
-	for i in $(seq 1 $num_ctr); do
-		output+=$(triton inst create -n {{shortId}}$name_modifier $image $ctr_package $prd_params -t triton.cns.services="ctr-$cluster_id" -m tag="ctr" &)
+	for i in $(seq 1 "$num_ctr"); do
+		output+=$(triton inst create -n {{shortId}}"$name_modifier" "$image" "$ctr_package" $prd_params -t triton.cns.services="ctr-$cluster_id" -m tag="ctr" &)
 		output+="\n"
 	done
 	wait
 
-	echo -e "$output"
+	printf "%s" "$output"
 }
 
 wrk() {
-	local output=""
+	output=""
 
 	echo "creating data plane members:"
 
-	for i in $(seq 1 $num_wrk); do
-		output+=$(triton inst create -n {{shortId}}$name_modifier $image $wrk_package $prd_params -m tag="wrk" --nic ipv4_uuid="$network" &)
+	for i in $(seq 1 "$num_wrk"); do
+		output+=$(triton inst create -n {{shortId}}"$name_modifier" "$image" "$wrk_package" $prd_params -m tag="wrk" --nic ipv4_uuid="$network" &)
 		output+="\n"
 	done
 	wait
 
-	echo -e "$output"
+	printf "%s" "$output"
 }
 
 dev_env() {
 	echo "Select a package size for your instance:"
-	read -p "Press enter to continue"
+	printf "Press enter to continue"
+	read -r _
 
 	dev_package=$(triton package ls | fzf --header='CTRL-c or ESC to quit' --layout=reverse-list | awk '{print $1}')
 
 	echo "Creating single control plane:"
 
-	triton inst create -n "{{shortId}}$name_modifier" $image $dev_package $dev_params -m tag=dev -t triton.cns.services=dev-$cluster_id
+	triton inst create -n {{shortId}}"$name_modifier" "$image" "$dev_package" $dev_params -m tag="dev" -t triton.cns.services="dev-$cluster_id"
 }
 
 prd_env() {
-	local choice=false
+	choice=false
 
 	while [ "$choice" = false ]; do
 		echo "How many control plane members would you like to create? (Choose 3, 5, 7, or 9)"
-		read -p "Enter number of members: " num_ctr
+		printf "Enter number of members: "
+		read -r num_ctr
 
-		if [[ "$num_ctr" == "3" || "$num_ctr" == "5" || "$num_ctr" == "7" || "$num_ctr" == "9" ]]; then
+		if [ "$num_ctr" = "3" ] || [ "$num_ctr" = "5" ] || [ "$num_ctr" = "7" ] || [ "$num_ctr" = "9" ]; then
 			choice=true
 		else
 			echo "Invalid choice. Please enter 3, 5, 7, or 9."
@@ -87,7 +81,8 @@ prd_env() {
 	done
 
 	echo "How many data plane members would you like to create? (Choose 1-99)"
-	read -p "Enter number of members: " num_wrk
+	printf "Enter number of members: "
+	read -r num_wrk
 
 	ctr_package=$(triton package ls | fzf --header='please select a package size for your control-plane instances. CTRL-c or ESC to quit' --layout=reverse-list | awk '{print $1}')
 	wrk_package=$(triton package ls | fzf --header='please select a package size for your data-plane instances. CTRL-c or ESC to exit' --layout=reverse-list | awk '{print $1}')
@@ -98,27 +93,46 @@ prd_env() {
 
 ls_cluster() {
 	clusterids=""
-	clusterids=$(triton inst ls -Hoshortid tag.cluster="*" | while read -r id; do triton inst tag get "$id" cluster; done | sort | uniq)
-	for i in $clusterids; do echo -e "cluster-id: $i\ninstances:" && triton inst ls -H tag.cluster="$i"; done
+	clusterids=$(triton inst ls -Hoshortid tag.cluster="*" | while read -r id; do triton inst tag get "$id" cluster; done | sort | uniq | grep -Ev "^0$")
+	for i in $clusterids; do printf "cluster-id: %s\ninstances:\n" "$i" && triton inst ls -H tag.cluster="$i"; done
 }
 
 rm_cluster() {
-	echo -e "current clusters:\n"
+	printf "current clusters:\n"
 	ls_cluster
 
-	read -p "Enter the Cluster-ID you'd like to delete: " cluster_id
+	printf "Enter the Cluster-ID you'd like to delete: "
+	read -r cluster_id
 	instances=$(triton inst ls -Hoshortid tag.cluster="$cluster_id")
 
 	if [ -n "$instances" ]; then
-		echo -e "\nDeleted Instances:"
+		printf "\nDeleted Instances:\n"
 		echo "$instances" | xargs -I {} triton inst rm -f {}
 	else
 		echo "No instances to delete"
 	fi
 }
 
+grab_kubeconfig() {
+	printf "current clusters:\n"
+	ls_cluster
+
+	printf "Enter the Cluster-ID you'd like to grab your kubeconfig from: "
+	read -r cluster_id
+
+	triton inst metadata get $(triton inst ls -Hoshortid tag.triton.cns.services="init-$cluster_id,ctr-$cluster_id") admin.conf
+
+}
+
 main() {
 	suffix
+
+	account=$(triton account get | grep -e 'id:' | sed -e 's/id:\ //') # account UUID
+	network=$(triton network ls -Hoid public=false)                    # Fabric Network UUID
+	kubernetes_version="1.29.8"
+	cluster_id=$(uuidgen | cut -d - -f1 | tr '[:upper:]' '[:lower:]')
+	prd_params="-b bhyve --cloud-config configs/cloud-init -t cluster=$cluster_id -m cluster=$cluster_id -m account=$account -m k8ver=$kubernetes_version"
+	dev_params="-b bhyve --cloud-config configs/cloud-init -t cluster=$cluster_id -m cluster=$cluster_id -m account=$account -m k8ver=$kubernetes_version"
 
 	echo "Would you like a Development or Production environment? (dev/prod)"
 	read -r environment
@@ -143,6 +157,7 @@ case "$ACTION" in
 "up") main ;;
 "down") rm_cluster ;;
 "show_clusters") ls_cluster ;;
+"kubeconfig") grab_kubeconfig ;;
 "upgrade") echo "Not added yet" ;;
 *) echo "Invalid action. Use 'up' or 'down'" usage ;;
 esac
