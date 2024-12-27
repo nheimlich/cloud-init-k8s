644a225b2fb6e588fccd3d151d4a262d44cfa002
Nolan Heimlich <76967780+nheimlich@users.noreply.github.com>
2024-12-27 00:22:33 -0500

    removal of fzf and restructuring of tk8s

--- tk8s.sh
+++ tk8s.sh
@@ -1,87 +1,210 @@
 #!/usr/bin/env sh
 
 command -v triton >/dev/null 2>&1 || echo >&2 "I require the triton cli before running"
-command -v fzf >/dev/null 2>&1 || echo >&2 "I require the fzf cli before running."
 
 usage() {
-	echo "Usage: $0 <action> [OPTIONS]"
-	echo " up      -- create kubernetes cluster"
-	echo " down    -- destroy a kubernetes cluster"
-	echo " ls      -- show existing clusters"
-	echo " config  -- get kubeconfig from an existing cluster"
-	echo " upgrade -- upgrade Clusters to a new version"
-	echo " bastion -- manage a trk8s bastion host"
-	echo " clb     -- manage cloud load balancer services"
-	exit 1
+	case "$1" in
+	main)
+		echo "Usage: $0 <action> [OPTIONS]"
+		echo " up      -- create kubernetes cluster"
+		echo " down    -- destroy a kubernetes cluster"
+		echo " ls      -- show existing clusters"
+		echo " config  -- get kubeconfig from an existing cluster"
+		echo " upgrade -- upgrade Clusters to a new version"
+		echo " bastion -- manage a trk8s bastion host"
+		echo " clb     -- manage cloud load balancer services"
+		exit 1
+		;;
+	up)
+		echo "Usage: $0 up [-i] [-d]"
+		echo "  -i              Run in interactive mode. (default)"
+		echo "  -d              Delete an existing cluster."
+		echo "  -h              Show this help message."
+		exit 1
+		;;
+	down)
+		echo "Usage: $0 down [-i] [-c cluster]"
+		echo "  -i              Run in interactive mode. (default)"
+		echo "  -c              Delete a specific cluster."
+		echo "  -h              Show this help message."
+		exit 1
+		;;
+	bastion)
+		echo "Usage: $0 bastion [-i] [-d] [-p package] [-g image]"
+		echo "  -i              Run in interactive mode."
+		echo "  -d              Delete an existing bastion instance."
+		echo "  -p package      Specify the bastion package."
+		echo "  -g image        Specify the bastion image."
+		echo "  -h              Show this help message."
+		exit 1
+		;;
+	clb)
+		echo "Usage: $0 clb [-i] [-d] [-p package] [-g image]"
+		echo "  -i              Run in interactive mode."
+		echo "  -d              Delete an existing bastion instance."
+		echo "  -c cluster      Specify the associated cluster."
+		echo "  -p package      Specify the clb package."
+		echo "  -e ext_uuid     Specify the external network UUID."
+		echo "  -n in_uuid      Specify the internal network UUID."
+		echo "  -r replicas     Specify the number of replicas."
+		echo "  -f fe_app       Specify the frontend app port."
+		echo "  -b be_app       Specify the backend app port."
+		echo "  -x fe_ssl       Specify the frontend SSL port."
+		echo "  -y be_ssl       Specify the backend SSL port."
+		echo "  -h              Show this help message."
+		exit 1
+		;;
+	*)
+		echo "Invalid usage. Run '$0 main' for the list of commands."
+		exit 1
+		;;
+	esac
 }
 
-suffix() {
-	printf "For an instance suffix, please type it now (Example: {{shortId}}.suffix), or press enter to skip:\n"
-	read -r suffix
+selection() {
+	printf "\nPlease review the information above and confirm.\n"
+	printf "Would you like to proceed? (yes/no)\n"
+	read -r confirm
+	case "$confirm" in
+	"yes" | "y") ;;
+	"no" | "n") exit 1 ;;
+	*) echo "Invalid Input, please choose yes/no" && exit 1 ;;
+	esac
+}
 
-	if [ -z "$suffix" ]; then
-		echo "Defaulting to no suffix..."
-		name_modifier=""
+prompt_for_input() {
+	local prompt="$1"
+	local var_name="$2"
+	local allow_empty="$3"
+
+	while true; do
+		printf "\n%s\n" "$prompt"
+		read -r input
+		if [ -n "$input" ] || [ "$allow_empty" = true ]; then
+			eval "$var_name='$input'"
+			break
+		else
+			echo "This value is required. Please try again."
+		fi
+	done
+}
+
+create_cluster() {
+
+	if [ "$environment" == "dev" ]; then
+		printf "\nCreating standalone instance:\n"
+		triton inst create -n {{shortId}}"$name_modifier" "$image" "$dev_package" $dev_params -m tag="dev" -t triton.cns.services="dev-$cluster_id"
+
+	elif [ "$environment" == "prod" ]; then
+		printf "\nCreating control-plane members:\n"
+		triton inst create -n {{shortId}}"$name_modifier" "$image" "$ctr_package" $prd_params -t triton.cns.services="init-$cluster_id,ctr-$cluster_id" -m "ctr_count=$num_ctr" -m "wrk_count=$num_wrk" -m tag="init" $ctr_interfaces
+		num_ctr=$((num_ctr - 1))
+		for i in $(seq 1 "$num_ctr"); do
+			triton inst create -n {{shortId}}"$name_modifier" "$image" "$ctr_package" $prd_params -t triton.cns.services="ctr-$cluster_id" -m tag="ctr" $ctr_interfaces
+		done
+		wait
+		printf "\nCreating data-plane members:\n"
+		for i in $(seq 1 "$num_wrk"); do
+			triton inst create -n {{shortId}}"$name_modifier" "$image" "$wrk_package" $prd_params -t triton.cns.services="wrk-$cluster_id" -t tritoncli.ssh.proxy="$(triton inst ls -Hoshortid tag.role=bastion)" -m tag="wrk" $wrk_interfaces
+		done
+		wait
 	else
-		name_modifier=".$suffix"
+		printf "Invalid environment type, please choose dev or prod\n" && exit 1
 	fi
 }
 
-dev_env() {
-	image=$(triton image ls type=zvol os=linux | sort -k2,2 -k3,3r | awk '!seen[$2]++' | fzf --header='please select a image for your kubernetes environment. CTRL-c or ESC to quit' --layout=reverse-list | awk '{print $1}')
+interactive_k8s() {
+	dev_opts() {
+		prompt_for_input "Would you like this development instance to be external? (yes/no)?" external false
+		case "$external" in
+		"n" | "no") external="" ;;
+		"y" | "yes") triton network ls -l && prompt_for_input "Enter the External Network UUID:" ext_uuid true ;;
+		*) echo "Invalid Input, please choose yes/no" && exit 1 ;;
+		esac
+		triton package ls
+		prompt_for_input "Select a package size for your instance:" dev_package false
+	}
+	prod_opts() {
+		prompt_for_input "Which Instances would you like to have External Interfaces (ctr, wrk, both, none)?" external false
+		triton package ls
+		prompt_for_input "Select a package size for your control-plane instances:" ctr_package false
+		prompt_for_input "Select a package size for your data-plane instances:" wrk_package false
+		choice=false
+		while [ "$choice" = false ]; do
+			prompt_for_input "How many control plane members would you like to create? (Choose 3, 5, 7, or 9)" num_ctr false
 
-	echo "Select a package size for your instance:"
-	printf "Press enter to continue"
-	read -r _
-
-	dev_package=$(triton package ls | fzf --header='CTRL-c or ESC to quit' --layout=reverse-list | awk '{print $1}')
-
-	echo "Creating single control plane:"
-
-	triton inst create -n {{shortId}}"$name_modifier" "$image" "$dev_package" $dev_params -m tag="dev" -t triton.cns.services="dev-$cluster_id"
-}
-
-prd_env() {
-	choice=false
-
-	image=$(triton image ls type=zvol os=linux | sort -k2,2 -k3,3r | awk '!seen[$2]++' | fzf --header='please select a image for your kubernetes environment. CTRL-c or ESC to quit' --layout=reverse-list | awk '{print $1}')
-
-	while [ "$choice" = false ]; do
-		echo "How many control plane members would you like to create? (Choose 3, 5, 7, or 9)"
-		printf "Enter number of members: "
-		read -r num_ctr
-
-		if [ "$num_ctr" = "3" ] || [ "$num_ctr" = "5" ] || [ "$num_ctr" = "7" ] || [ "$num_ctr" = "9" ]; then
-			choice=true
-		else
-			echo "Invalid choice. Please enter 3, 5, 7, or 9."
+			if [ "$num_ctr" = "3" ] || [ "$num_ctr" = "5" ] || [ "$num_ctr" = "7" ] || [ "$num_ctr" = "9" ]; then
+				choice=true
+			else
+				echo "Invalid choice. Please enter 3, 5, 7, or 9."
+			fi
+		done
+		prompt_for_input "How many data plane members would you like to create? (Choose 1-99)" num_wrk false
+		if [ "$external" == "ctr" ] || [ "$external" == "both" ] || [ "$external" == "wrk" ]; then
+			triton network ls -l
+			prompt_for_input "Enter the External Network UUID:" ext_uuid true
 		fi
-	done
+	}
+	prompt_for_input "For an instance suffix, please type it now (Example: {{shortId}}.suffix), or press enter to skip:" suffix true
+	triton image ls type=zvol os=linux | sort -k2,2 -k3,3r | awk '!seen[$2]++'
+	prompt_for_input "Select an image for your kubernetes environment:" image false
+	triton network ls -l
+	prompt_for_input "Enter the Internal Network UUID:" in_uuid false
+	prompt_for_input "Would you like a Development or Production environment? (dev/prod)" environment false
+	case "$environment" in
+	"dev") dev_opts ;;
+	"prod") prod_opts ;;
+	*) echo "Invalid Input, please choose dev/prod" && exit 1 ;;
+	esac
 
-	echo "How many data plane members would you like to create? (Choose 1-99)"
-	printf "Enter number of members: "
-	read -r num_wrk
+	if [ -n "$suffix" ]; then
+		name_modifier=".$suffix"
+	else
+		name_modifier=""
+	fi
 
-	ctr_package=$(triton package ls | fzf --header='please select a package size for your control-plane instances. CTRL-c or ESC to quit' --layout=reverse-list | awk '{print $1}')
-	wrk_package=$(triton package ls | fzf --header='please select a package size for your data-plane instances. CTRL-c or ESC to exit' --layout=reverse-list | awk '{print $1}')
+	case "$external" in
+	"ctr")
+		ctr_interfaces="--nic ipv4_uuid=$in_uuid --nic ipv4_uuid=$ext_uuid"
+		wrk_interfaces="--nic ipv4_uuid=$in_uuid"
+		;;
+	"wrk")
+		wrk_interfaces="--nic ipv4_uuid=$in_uuid --nic ipv4_uuid=$ext_uuid"
+		ctr_interfaces="--nic ipv4_uuid=$in_uuid"
+		;;
+	"both")
+		ctr_interfaces="--nic ipv4_uuid=$in_uuid --nic ipv4_uuid=$ext_uuid"
+		wrk_interfaces="$ctr_interfaces"
+		;;
+	"dev") dev_interfaces="--nic ipv4_uuid=$in_uuid --nic ipv4_uuid=$ext_uuid" ;;
+	esac
 
-	echo "creating control-plane members:"
+	# current setup information for confirmation
+	printf "\nCluster: %s\n" $cluster_id
+	if [ -n "$suffix" ]; then
+		echo "Suffix: $suffix"
+	fi
+	if [ "$external" == "ctr" ] || [ "$external" == "both" ] || [ "$external" == "wrk" ] || [ "$external" == "dev" ]; then
+		echo "External: true"
+		echo "External Network UUID: $ext_uuid"
+	fi
 
-	triton inst create -n {{shortId}}"$name_modifier" "$image" "$ctr_package" $prd_params -t triton.cns.services="init-$cluster_id,ctr-$cluster_id" -m "ctr_count=$num_ctr" -m "wrk_count=$num_wrk" -m tag="init"
+	if [ "$environment" == "dev" ]; then
+		echo "Standalone package: $dev_package"
+	else
+		echo "Control-plane package: $ctr_package"
+		echo "Data-plane package: $wrk_package"
+		echo "Number of control-plane instances: $num_ctr"
+		echo "Number of data-plane instances: $num_wrk"
+	fi
 
-	num_ctr=$((num_ctr - 1))
+	echo "Internal Network UUID: $in_uuid"
+	echo "Environment: $environment"
+	echo "Image: $image"
 
-	for i in $(seq 1 "$num_ctr"); do
-		triton inst create -n {{shortId}}"$name_modifier" "$image" "$ctr_package" $prd_params -t triton.cns.services="ctr-$cluster_id" -m tag="ctr"
-	done
-	wait
+	selection
 
-	echo "creating data plane members:"
-
-	for i in $(seq 1 "$num_wrk"); do
-		triton inst create -n {{shortId}}"$name_modifier" "$image" "$wrk_package" $prd_params -t triton.cns.services="wrk-$cluster_id" -t tritoncli.ssh.proxy="$(triton inst ls -Hoshortid tag.role=bastion)" -m tag="wrk" --nic ipv4_uuid="$network"
-	done
-	wait
+	create_cluster
 }
 
 ls_cluster() {
@@ -115,11 +238,37 @@ ls_cluster() {
 }
 
 rm_cluster() {
-	ls_cluster
+	unset interactive cluster_id instances
 
-	printf "Enter the Cluster-ID you'd like to delete: "
-	read -r cluster_id
+	# Default values for flag options
+	cluster_id=""       # Cluster ID
+	instances=""        # Instances to delete
+	interactive="false" # Interactive Flag
+
+	if [ "$#" -eq 0 ]; then
+		interactive="true"
+	elif [ "$#" -ne 0 ]; then
+		while getopts "c:hi" opt; do
+			case "$opt" in
+			c) cluster_id="$OPTARG" ;; # Bastion Package
+			i) interactive="true" ;;   # Interactive mode flag
+			h) usage down ;;
+			*) usage down ;;
+			esac
+		done
+	fi
+
+	if [ "$interactive" == "true" ]; then
+		ls_cluster
+		printf "Enter the Cluster-ID you'd like to delete: "
+		read -r cluster_id
+	fi
+
 	instances=$(triton inst ls -Hoshortid tag.cluster="$cluster_id")
+
+	printf "Instances to be deleted:\n%s" "$instances"
+
+	selection
 
 	if [ -n "$instances" ]; then
 		printf "\nDeleted Instances:\n"
@@ -148,19 +297,6 @@ grab_kubeconfig() {
 	fi
 }
 
-interactive() {
-	suffix
-	echo "Would you like a Development or Production environment? (dev/prod)"
-	read -r environment
-
-	case "$environment" in
-	"prod") prd_env ;;
-	"dev") dev_env ;;
-	*) echo "Invalid Input, please select an environment" && exit 1 ;;
-	esac
-
-}
-
 cloud_load_balancer() {
 	unset fe_ctr be_ctr fe_app be_app fe_ssl be_ssl cluster package replicas interactive ext_uuid in_uuid interactive deletion
 
@@ -179,23 +315,6 @@ cloud_load_balancer() {
 	in_uuid=""          # Internal network UUID
 	deletion="false"    # Deletion of CLB instances
 
-	usage() {
-		echo "Usage: $0 clb [-i] [-d] [-p package] [-g image]"
-		echo "  -i              Run in interactive mode."
-		echo "  -d              Delete an existing bastion instance."
-		echo "  -c cluster      Specify the associated cluster."
-		echo "  -p package      Specify the clb package."
-		echo "  -e ext_uuid     Specify the external network UUID."
-		echo "  -n in_uuid      Specify the internal network UUID."
-		echo "  -r replicas     Specify the number of replicas."
-		echo "  -f fe_app       Specify the frontend app port."
-		echo "  -b be_app       Specify the backend app port."
-		echo "  -x fe_ssl       Specify the frontend SSL port."
-		echo "  -y be_ssl       Specify the backend SSL port."
-		echo "  -h              Show this help message."
-		exit 1
-	}
-
 	# Parse options
 	while getopts "c:p:e:n:r:f:b:x:y:hid" opt; do
 		case "$opt" in
@@ -210,8 +329,8 @@ cloud_load_balancer() {
 		y) be_ssl="$OPTARG" ;;   # Backend SSL port
 		i) interactive="true" ;; # Interactive mode flag
 		d) deletion="true" ;;    # Deletion of CLB instances
-		h) usage ;;
-		*) usage ;;
+		h) usage clb ;;
+		*) usage clb ;;
 		esac
 	done
 
@@ -219,7 +338,7 @@ cloud_load_balancer() {
 	shift $((OPTIND - 1))
 
 	if [ "$OPTIND" -eq 1 ]; then
-		usage
+		usage clb
 	fi
 
 	if [ "$deletion" == "true" ]; then
@@ -291,6 +410,12 @@ cloud_load_balancer() {
 		echo "No existing load balancer found, creating a new one..."
 	fi
 
+	int_cns_suffix=$(triton cloudapi "/my/networks/$in_uuid" | grep -o '"svc\.[^",]*' | sed 's/^"//;s/",*$//')
+	ext_cns_suffix=$(triton cloudapi "/my/networks/$ext_uuid" | grep -o '"svc\.[^",]*' | sed 's/^"//;s/",*$//')
+
+	app_cns="wrk-$cluster.$int_cns_suffix"
+	ctr_cns="ctr-$cluster.$int_cns_suffix"
+
 	# current setup information for confirmation
 	echo "Cluster: $cluster"
 	echo "Package: $package"
@@ -301,21 +426,11 @@ cloud_load_balancer() {
 	echo "Backend Kube API port: $be_ctr"
 	echo "Frontend SSL port: $fe_ssl"
 	echo "Backend SSL port: $be_ssl"
+	echo "Internal CNS: $app_cns"
+	echo "External CNS: $ctr_cns"
 	echo "Interactive: $interactive"
 
-	printf "\nWould you like to proceed?\n"
-	read -r selection
-	case "$selection" in
-	"n" | "no") exit 1 ;;
-	"y" | "yes") continue ;;
-	*) echo "Invalid Input, please choose yes/no" && exit 1 ;;
-	esac
-
-	int_cns_suffix=$(triton cloudapi "/my/networks/$in_uuid" | grep -o '"svc\.[^",]*' | sed 's/^"//;s/",*$//')
-	ext_cns_suffix=$(triton cloudapi "/my/networks/$ext_uuid" | grep -o '"svc\.[^",]*' | sed 's/^"//;s/",*$//')
-
-	app_cns="wrk-$cluster.$int_cns_suffix"
-	ctr_cns="ctr-$cluster.$int_cns_suffix"
+	selection
 
 	# Create load balancer instances
 	for i in $(seq 1 "$replicas"); do
@@ -337,16 +452,6 @@ bastion() {
 	bst_package=""      # Bastion Package
 	bst_image=""        # Bastion Image
 
-	usage() {
-		echo "Usage: $0 bastion [-i] [-d] [-p package] [-g image]"
-		echo "  -i              Run in interactive mode."
-		echo "  -d              Delete an existing bastion instance."
-		echo "  -p package      Specify the bastion package."
-		echo "  -g image        Specify the bastion image."
-		echo "  -h              Show this help message."
-		exit 1
-	}
-
 	# Parse options
 	while getopts "p:g:hid" opt; do
 		case "$opt" in
@@ -354,8 +459,8 @@ bastion() {
 		g) bst_image="$OPTARG" ;;   # Bastion Image
 		i) interactive="true" ;;    # Interactive mode flag
 		d) deletion="true" ;;       # Deletion of Bastion Instance
-		h) usage ;;
-		*) usage ;;
+		h) usage bastion ;;
+		*) usage bastion ;;
 		esac
 	done
 
@@ -363,7 +468,7 @@ bastion() {
 	shift $((OPTIND - 1))
 
 	if [ "$OPTIND" -eq 1 ]; then
-		usage
+		usage bastion
 	fi
 
 	if [ "$deletion" == "true" ]; then
@@ -407,15 +512,32 @@ bastion() {
 	triton inst create -n {{shortId}}-bastion "$bst_image" "$bst_package" -t triton.cns.services="bastion" -t role="bastion"
 }
 
-main() {
+up_cluster() {
 	account=$(triton account get | grep -e 'id:' | sed -e 's/id:\ //') # account UUID
-	network=$(triton network ls -Hoid public=false)                    # Fabric Network UUID
 	kubernetes_version="1.29.8"
 	cluster_id=$(uuidgen | cut -d - -f1 | tr '[:upper:]' '[:lower:]')
 	prd_params="-b bhyve --cloud-config configs/cloud-init -t cluster=$cluster_id -m cluster=$cluster_id -m account=$account -m k8ver=$kubernetes_version"
 	dev_params="-b bhyve --cloud-config configs/cloud-init -t cluster=$cluster_id -m cluster=$cluster_id -m account=$account -m k8ver=$kubernetes_version"
 
-	interactive
+	if [ "$#" -eq 0 ]; then
+		interactive_k8s
+	elif [ "$#" -ne 0 ]; then
+		while getopts "hid" opt; do
+			case "$opt" in
+			i) interactive_k8s ;; # Interactive mode flag
+			d) rm_cluster ;;      # Deletion of tk8s instances
+			h) usage up ;;
+			*) usage up ;;
+			esac
+		done
+	fi
+
+	shift $((OPTIND - 1))
+
+	if [ "$OPTIND" -eq 1 ]; then
+		usage up
+	fi
+
 }
 
 ACTION="$1"
@@ -423,12 +545,12 @@ ACTION="$1"
 shift
 
 case "$ACTION" in
-"up") main ;;
-"down") rm_cluster ;;
+"up") up_cluster "$@" ;;
+"down") rm_cluster "$@" ;;
 "ls") ls_cluster ;;
 "config") grab_kubeconfig ;;
 "upgrade") printf "not implemented yet\n" ;;
 "bastion") bastion "$@" ;;
 "clb") cloud_load_balancer "$@" ;;
-*) printf "invalid action.\n" && usage ;;
+*) printf "invalid action.\n" && usage main ;;
 esac
